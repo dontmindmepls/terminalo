9f0def8921e597348eb973b9992ebc93be627a45
dontmindmepls <71087781+dontmindmepls@users.noreply.github.com>
2026-05-22 11:02:25 -0700

    Create main.c
    where the base of the terminal will go ; separate file will be made for the functions

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define OPERATION_SUCCESSFUL 0
+#define OPERATION_FAILED 1
+#define TERMINAL_EXIT 2
+
+int main()
+{
+char TEXT_PLACEMENT[250] = "";
+  
+while (strlen(TEXT_PLACEMENT) == 0)
+  {
+    printf("user $");
+    if (strcmp(TEXT_PLACEMENT, "exit TERMINAL"));
+      break;
+
+    TEXT_PLACEMENT = 0;
+  }
+  return TERMINAL_EXIT;
+}
