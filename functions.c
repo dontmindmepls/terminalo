466f2170cbd822083c79fdca8d88667c4b92685a
dontmindmepls <71087781+dontmindmepls@users.noreply.github.com>
2026-05-22 11:06:40 -0700

    Create functions.c

---
+++ functions.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+// to be debugged...
+
+void echo (char * echo_string)
+{
+  printf("\n%s\n", echo_string);
+}
+
+void ls (char * filepath)
+{
+  FILE 
+}
