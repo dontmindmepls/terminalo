2a4c0b5d57991386257c4c99fef9d331518431f0
dontmindmepls <71087781+dontmindmepls@users.noreply.github.com>
2026-05-23 00:54:39 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+/* NOTES
+
+- later use malloc() or realloc() to rework the memory on the input string to allow for
+dynamic string size
+- place
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,22 +16,30 @@
 
 int main()
 {
-char TEXT_PLACEMENT[512] = "";
+char CHAR_INPUT[512] = "";
 char buffer[1024] = "";
   
 while (1)
   {
     printf("user $ ");
-    fgets();
-    if (strcmp(TEXT_PLACEMENT, "exit TERMINAL"));
+    fgets(CHAR_INPUT, sizeof(CHAR_INPUT), stdin);
+    CHAR_INPUT[strlen(CHAR_INPUT) - 1] = '\0';
+    
+    if (strcmp(CHAR_INPUT, "exit TERMINAL") == 0)
       break;
-
-    TEXT_PLACEMENT = 0;
   }
   exit(TERMINAL_EXIT);
 }
 
-/* NOTES / REVIEW
+/* REVIEW
 https://pubs.opengroup.org/onlinepubs/009695399/functions/fopen.html
 https://web.eng.fiu.edu/watsonh/EEL2880/FileIO-HW.pdf
+https://stackoverflow.com/questions/985051/what-is-the-purpose-of-fork
+https://www.geeksforgeeks.org/c/strcmp-in-c/
+https://www.w3schools.com/c/ref_string_strncmp.php
+https://www.geeksforgeeks.org/c/how-to-split-a-string-by-a-delimiter-in-c/
+https://www.geeksforgeeks.org/c/concatenating-two-strings-in-c/
+https://www.geeksforgeeks.org/c/c-define-preprocessor/
+https://www.geeksforgeeks.org/c/sprintf-in-c/
+https://www.geeksforgeeks.org/c/fgets-function-in-c/
 */
