18703a9981cfc93529dcf349fa20e8e705a97ae2
dontmindmepls <71087781+dontmindmepls@users.noreply.github.com>
2026-05-23 01:02:31 -0700

    Update and rename functions.c to functions.h

---
+++ functions.h
@@ -0,0 +1,13 @@
+#ifndef OPERATION_SUCCESS
+#define OPERATION_SUCCESS 0
+#endif
+
+#ifndef OPERATION_FAILED
+#define OPERATION_FAILED 1
+#endif
+
+#ifndef TERMINAL_EXIT
+#define TERMINAL_EXIT 2
+#endif
+
+void echo (char * echo_string);
